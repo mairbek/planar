bc491d62468697c90fce373da11b51298f52ba6d
Mairbek Khadikov <mkhadikov@gmail.com>
2011-08-22 08:35:31 +0300

    Hill climbing and Annealing algorithms implemented

--- optimization.py
+++ optimization.py
@@ -1,5 +1,6 @@
 import random
 import logging
+import math
 
 logger = logging.getLogger("Optimization")
 
@@ -29,4 +30,83 @@ def random_optimize(domain, cost_fun, points=1000, max_y=(10 ** 8), min_y=0):
 
 
 def hill_climb_optimize(domain, cost_fun):
-    pass
+    logger.debug("Going to perform hill climb optimization")
+
+    y = None
+    x = [random.randint(domain[i][0], domain[i][1])
+         for i in range(len(domain))]
+
+    while True:
+        neighbors = []
+        for j in range(len(domain)):
+            if x[j] > domain[j][0]:
+                n = x[0:j] + [x[j] + 1] + x[j + 1:]
+                neighbors.append(n)
+            if x[j] < domain[j][1]:
+                n = x[0:j] + [x[j] - 1] + x[j + 1:]
+                neighbors.append(n)
+
+        current = cost_fun(x)
+        best = current
+        for  j in range(len(neighbors)):
+            cost = cost_fun(neighbors[j])
+            logger.debug("Hill climb cost=%s", cost)
+            if cost < best:
+                best = cost
+                x = neighbors[j]
+
+        logger.debug("Hill climb: x=%s, y=%s", x, best)
+
+        if best == current or best == 0:
+            y = best
+            break
+
+    return x, y
+
+
+def annealing_optimize(domain, cost_function, T=1000000.0, cool=0.99999, step=1):
+    x = [float(random.randint(domain[i][0], domain[i][1]))
+         for i in range(len(domain))]
+    y = None
+
+    min = None
+
+    while T > 0.1:
+        i = random.randint(0, len(domain) - 1)
+
+        dir = random.randint(-step, step)
+
+        vecb = x[:]
+        vecb[i] += dir
+        if vecb[i] < domain[i][0]:
+            vecb[i] = domain[i][0]
+        elif vecb[i] > domain[i][1]:
+            vecb[i] = domain[i][1]
+
+        ea = cost_function(x)
+        eb = cost_function(vecb)
+
+        p = pow(math.e, (-eb - ea) / T)
+
+        if eb < ea or random.random() < p:
+            x = vecb
+            y = eb
+
+        if min is None:
+            min = {"x": x, "y": y}
+        elif min["y"] > y:
+            min["x"] = x
+            min["y"] = y
+
+        if not y:
+            break
+
+        T = T * cool
+
+        logger.debug("x = %s, y = %s", x, y)
+
+    if y > min["y"]:
+        x = min["x"]
+        y = min["y"]
+
+    return x, y
--- planar.py
+++ planar.py
@@ -1,60 +1,54 @@
 import logging
+import math
 import optimization
 from dataprovier import provide_graph
 import view
 
-def crosscount(coordinates, vertex, edge):
-    logger = logging.getLogger("Crosscount")
-    logger.debug("Current coordinates %s", coordinates)
+logger = logging.getLogger("planar")
+
+def crosscount(coordinates, vertex, edge, min_distance=50):
 
     loc = dict([(vertex[i], (coordinates[i * 2], coordinates[i * 2 + 1])) for i in range(0, len(vertex))])
-    logger.debug("locations %s", loc)
     total = 0
 
     for i in range(0, len(nodes)):
-        for j in range(i+1, len(nodes)):
-            logger.debug("%s %s", i, j)
+        for j in range(i + 1, len(nodes)):
 
             (x1, y1), (x2, y2) = loc[edge[i][0]], loc[edge[i][1]]
             (x3, y3), (x4, y4) = loc[edge[j][0]], loc[edge[j][1]]
 
-            logger.debug("first edge %s {%s , %s}", edge[i], (x1, y1), (x2, y2))
-            logger.debug("second edge %s {%s , %s}", edge[j], (x3, y3), (x4, y4))
             if are_crossing(x1, y1, x2, y2, x3, y3, x4, y4):
                 total += 1
+
+    for i in range(len(vertex)):
+        for j in range(i + 1, len(vertex)):
+            (x1, y1), (x2, y2) = loc[vertex[i]], loc[vertex[j]]
+
+            dist = math.sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
+
+            if dist < min_distance:
+                total += (1 - (dist / float(min_distance)))
+
     return total
 
 
 def are_crossing(x1, y1, x2, y2, x3, y3, x4, y4):
-    logger = logging.getLogger("are_crossing")
-
-#    logger.debug("x1 %s", x1)
-#    logger.debug("y1 %s", y1)
-#    logger.debug("x2 %s", x2)
-#    logger.debug("y2 %s", y2)
-#    logger.debug("x3 %s", x3)
-#    logger.debug("y3 %s", y3)
-#    logger.debug("x4 %s", x4)
-#    logger.debug("y4 %s", y4)
 
     den = 0.0 + (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
-    logger.debug("den %s", den)
 
-    if den == 0:
+    if not den:
+        # parallel
         return False
 
     ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / den
     ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / den
 
-    logger.debug("ua %s", ua)
-    logger.debug("ub %s", ub)
-
     return ua > 0 and ua < 1 and ub > 0 and ub < 1
 
 
-def cost_function(vertex, nodes):
+def cost_function(vertex, edges):
     def func(v):
-        return crosscount(v, vertex, nodes)
+        return crosscount(v, vertex, edges)
 
     return func
 
@@ -68,7 +62,7 @@ if __name__ == "__main__":
 
     logging.info(domain)
 
-    (result_x, result_y) = optimization.random_optimize(domain, cost_function(vertex, nodes))
+    (result_x, result_y) = optimization.annealing_optimize(domain, cost_function(vertex, nodes))
 
     logging.info("result (x=" + str(result_x) + " y=" + str(result_y) + ")")
 
